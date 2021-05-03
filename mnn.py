2b9a394ab810467413dda78a6c482e2c79e1021d
Sam Tredgett <sam.tredgett@gmail.com>
2021-05-03 14:55:56 +0100

    Fleshed out more of the classification problem; including the data set which has two clear clusters ready to be tested on the perceptron model once that has been built.

--- mnn.py
+++ mnn.py
@@ -1,3 +1,4 @@
+from sklearn.datasets import make_blobs
 import numpy as np
 class Operation():
     def __init__(self, input_nodes=[]):
@@ -118,6 +119,42 @@ print(result)
 
 
 # Classification Section
+# Activation Function
+
+import matplotlib.pyplot as plt
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+sample_z = np.linspace(-10,10,100)
+sample_a = sigmoid(sample_z)
+
+plt.plot(sample_z,sample_a)
+plt.show()
+
+class Sigmoid(Operation):
+    def __init__(self, z):
+        super().__init__([z])
+    def compute(self,z_val):
+        return 1 / (1 + np.exp(-z))
+
+data = make_blobs(n_samples=50, n_features=2,centers=2,random_state=75)
+
+features = data[0]
+labels = data[1]
+
+plt.scatter(features[:,0],features[:,1])
+plt.show()
+
+
+x = np.linspace(0,11,10)
+y = -x
+
+plt.scatter(features[:,0],features[:,1],c=labels,cmap='coolwarm')
+plt.plot(x,y)
+plt.show()
+
+
 
 
 
